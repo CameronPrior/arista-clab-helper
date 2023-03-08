b0216bba871e3ffbe8c12d729fe7cf6b6ee664df
CameronPrior <cameron.prior85@gmail.com>
2023-03-08 08:05:35 +0000

    Error handling for docker API

--- clab_helper.py
+++ clab_helper.py
@@ -362,7 +362,19 @@ def select_ceos_image(info):
     dockerClient = docker.from_env()
 
     # Get a list of all Docker images
-    dockerImages = dockerClient.images.list()
+    try:
+        dockerImages = dockerClient.images.list()
+    except docker.errors.APIError as e:
+        print("----------------------------------------")
+        print("Missing cEOS Image")
+        print("----------------------------------------")
+        print("")
+        print("No CEOS Images found loaded into docker")
+        print(
+            "Please download a supported cEOS image and import it using the 'docker import' command"
+        )
+        input("Press any key to exit...")
+        terminate_script()
 
     # Create an empty list to hold image dictionaries
     imageDicts = []
@@ -374,17 +386,6 @@ def select_ceos_image(info):
                 imageDict = {"tags": [tag]}
                 imageDicts.append(imageDict)
                 break
-    if imageDicts == []:
-        print("----------------------------------------")
-        print("Missing cEOS Image")
-        print("----------------------------------------")
-        print("")
-        print("No CEOS Images found loaded into docker")
-        print(
-            "Please download a supported cEOS image and import it using the 'docker import' command"
-        )
-        input("Press any key to exit...")
-        terminate_script()
 
     # Convert the list of image dictionaries to a JSON string and then back to a list of dictionaries
     jsonStr = json.dumps(imageDicts)
